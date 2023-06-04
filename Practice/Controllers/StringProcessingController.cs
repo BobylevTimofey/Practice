a1341b61bd3cf79af08a8dc6b58f22b4f560be3d
BobylevTimofey <71931806+BobylevTimofey@users.noreply.github.com>
2023-06-04 14:09:27 +0500

    Выполнил первое задание. Написал сервис обработки строк, unit-тесты к нему и post-запрос для работы с сервисом.

---
+++ Practice/Controllers/StringProcessingController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Practice.Services;
+
+namespace Practice.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StringProcessingController : Controller
+    {
+        private readonly StringProcessingService stringProcessing;
+
+        public StringProcessingController(StringProcessingService stringProcessing)
+        {
+            this.stringProcessing = stringProcessing;
+        }
+
+        [HttpPost]
+        public string? Index([FromBody] string originalString)
+        {
+            return stringProcessing.ProcessString(originalString);
+        }
+    }
+}
